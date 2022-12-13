35061902a072cba2c1b71f7e57604ff4d81c260d
swhwang <83943446+swhwang81@users.noreply.github.com>
2022-12-13 15:16:36 +0900

    Add files via upload

--- pages/3 Prediction.py
+++ pages/3 Prediction.py
@@ -12,7 +12,32 @@ st.markdown("""
 """,unsafe_allow_html=True)
 ################################################
 
-st.markdown("# Predict")
-#st.sidebar.markdown("# Prediction") 
+st.markdown("""
+    <span style ='font-size:34px; font-weight: bold;'> Predict  </span>
+""", unsafe_allow_html=True)
+st.image('icon01.png', width=150)
 
 st.write("this is prediction.")
+st.write("this is to apply a machine learning model. currently this model is for iris classification.")
+st.write("But, I'll change a model for my nursing app, soon.")
+
+
+st.write("## dataset")
+data = pd.read_csv("iris.csv")
+data
+
+st.write("## model test")
+
+model = pickle.load(open('iri.pkl','rb'))
+arr = np.array([[5.9,3.0,5.0,1.8]])
+st.write(arr)
+
+pred = model.predict(arr)    
+
+
+if pred == 0:
+    st.write("### predict result: ... Setosa !!",  str(pred))
+elif pred == 1:
+    st.write("### predict result: ... Versicolor !!", str(pred))
+elif pred == 2:
+    st.write("### predict result: ... Virginica !!",  str(pred))
